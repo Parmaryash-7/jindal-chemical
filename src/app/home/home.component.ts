95ffd69268fcc62ae234c2b967453fd6a2b5ab58
GadaniPanth <gadanipanth@gmail.com>
2025-05-08 16:49:19 +0530

    added swiper

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper, { Mousewheel, Navigation, Pagination } from "swiper";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  swiper!: Swiper;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    Swiper.use([Navigation]);
+
+    this.swiper = new Swiper(".swiper-container", {
+      slidesPerView: "auto",
+      spaceBetween: 24,
+      speed: 500,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+      loop: true,
+      mousewheel: true,
+    });
   }
 
 }
