d9b75cbbad3201d75c1a8f50007b93e82b12bdbc
PARAS4105 <paras@coronation.in>
2025-05-09 18:16:57 +0530

    tab panel pending

--- src/app/expertise/expertise.component.ts
+++ src/app/expertise/expertise.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-expertise',
-  templateUrl: './expertise.component.html',
-  styleUrls: ['./expertise.component.css']
+  selector: "app-expertise",
+  templateUrl: "./expertise.component.html",
+  styleUrls: ["./expertise.component.css"],
 })
 export class ExpertiseComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
+  activeTab = "Chemistry";
 
-  ngOnInit() {
+  tabs = [
+    {
+      id: "Chemistry",
+      label: "Chemistry",
+      title: "Content for Tab 1",
+      content: "This is the content for the first tab.",
+      align: "left",
+    },
+    {
+      id: "Technologies",
+      label: "Technologies",
+       title: "Content for Tab 1",
+      content: "This is the content for the first tab.",
+      align: "left",
+    },
+    {
+      id: "R & D",
+      label: "R & D",
+       title: "Content for Tab 1",
+      content: "This is the content for the first tab.",
+      align: "left"
+    },
+  ];
+
+  selectTab(tabId: string) {
+    this.activeTab = tabId;
   }
-
 }
