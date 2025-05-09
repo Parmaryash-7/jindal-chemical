9f5d5e87e85632cce57194e12684a73bf8d700d4
GadaniPanth <gadanipanth@gmail.com>
2025-05-09 11:28:29 +0530

    menubar fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jindal-chemical';
+
+  private _isMenuOpen = false;
+  get isMenuOpen(): boolean {
+    return this._isMenuOpen;
+  }
+
+  set isMenuOpen(value: boolean) {
+    this._isMenuOpen = value;
+    document.body.style.overflow = value ? 'hidden' : '';
+  }
+
+  toggleMenu(e: any) {
+    // if (window.outerWidth <= 1280) {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('object');
+    // }
+  }
+
+  ngOnInit() {
+    document.addEventListener('keydown', () => {
+      this.isMenuOpen = false;
+    })
+  }
 }
